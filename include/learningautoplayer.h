f0fb5a5aff5807dd6cfec1133568e6b96e84001a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-02-17 19:39:37 -0600

    Update more WeightedAutoPlayer functions

--- include/learningautoplayer.h
+++ include/learningautoplayer.h
@@ -2,19 +2,28 @@
 #define LEARNING_AUTO_PLAYER_H_DDFH92SDJFIUWEFH
 #include "iplayer.h"
 #include "weightedautoplayer.h"
-#include <string_view>
+#include "roundinfo.h"
 
+#include <string_view>
+#include <vector>
+
+using RoundData_t = std::vector<IndividualRoundInfo>;
 class LearningAutoPlayer : IPlayer
 {
 private:
 	WeightedAutoPlayer m_weightedPlayer;
-
+	RoundData_t m_roundData{};
+	
+	double oponentRockCount{0};
+	double oponentPaperCount{0};
+	double oponentScissorsCount{0};
+	void updateMoveCounts(Move& move);
 public:
 	LearningAutoPlayer(const std::string_view name);
-	virtual Move doTurn() = 0;
-	virtual std::string_view getName() = 0;
-	virtual int getScore() = 0;
-	virtual void addScore(int scoreAddition) = 0;
-	virtual void addOpponentRoundData(std::string_view name, Move move) = 0;
+	virtual Move doTurn() override;
+	virtual std::string_view getName() override;
+	virtual int getScore() override;
+	virtual void addScore(int scoreAddition) override;
+	virtual void addOpponentRoundData(std::string_view name, Move move) override;
 };
 #endif
